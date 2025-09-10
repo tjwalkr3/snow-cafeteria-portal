f38823593b45ffe673aa344c629f62ea064d656a
sarah-strawberries <blue.howrse@gmail.com>
2025-09-10 09:16:27 -0600

    added Ingredient class

---
+++ Cafeteria.Shared/Ingredient.cs
@@ -0,0 +1,17 @@
+﻿namespace Cafeteria.Shared;
+
+public class Ingredient
+{
+    #region Properties
+    public string Name { get; private set; }
+    public decimal Price { get; private set; }
+    #endregion
+
+    #region Constructor
+    public Ingredient(string name, decimal price)
+    {
+        Name = name;
+        Price = price;
+    }
+    #endregion
+}
