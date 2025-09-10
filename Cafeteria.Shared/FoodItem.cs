d7f9f0b4cd369fb166d53f814b0fb22e79895c16
sarah-strawberries <blue.howrse@gmail.com>
2025-09-10 09:19:34 -0600

    added todo comments

--- Cafeteria.Shared/FoodItem.cs
+++ Cafeteria.Shared/FoodItem.cs
@@ -4,6 +4,8 @@ public class FoodItem
 {
     #region Properties
     public IReadOnlyDictionary<IngredientType, Ingredient>? Ingredients { get; }
+    // TODO: add price calculator and properties for different pieces of price
+    //       also add properties for name, description, image url, etc.
     #endregion
 
     #region Constructor
--- Cafeteria.Shared/Ingredient.cs
+++ Cafeteria.Shared/Ingredient.cs
@@ -5,6 +5,7 @@ public class Ingredient
     #region Properties
     public string Name { get; private set; }
     public decimal Price { get; private set; }
+    // TODO: add property for image url
     #endregion
 
     #region Constructor
