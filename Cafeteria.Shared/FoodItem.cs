7f95308f5452b44bd36a1460275d55977aa22cb5
sarah-strawberries <blue.howrse@gmail.com>
2025-09-10 20:12:02 -0600

    finished FoodItem class

--- Cafeteria.Shared/FoodItem.cs
+++ Cafeteria.Shared/FoodItem.cs
@@ -2,15 +2,37 @@ namespace Cafeteria.Shared;
 
 public class FoodItem
 {
-    #region Properties
+    #region Public Properties
+    public string Name { get; init; }
+    public string? Description { get; init; }
+    public string ImageUrl { get; init; }
     public IReadOnlyDictionary<IngredientType, Ingredient>? Ingredients { get; }
-    // TODO: add price calculator and properties for different pieces of price
-    //       also add properties for name, description, image url, etc.
+    public decimal Price
+    {
+        get
+        {
+            if (extrasPrice is not null)
+            {
+                return basePrice + extrasPrice.Value;
+            }
+            else return basePrice;
+        }
+    }
+    #endregion
+
+    #region Private Properties
+    private decimal basePrice { get; init; }
+    private decimal? extrasPrice { get; init; }
     #endregion
 
     #region Constructor
-    public FoodItem(IReadOnlyDictionary<IngredientType, Ingredient>? ingredients = null)
+    public FoodItem(string name, string imgUrl, decimal itemPrice, decimal extrasPrice, string? description = null, IReadOnlyDictionary<IngredientType, Ingredient>? ingredients = null)
     {
+        Name = name;
+        Description = description;
+        ImageUrl = imgUrl;
+        basePrice = itemPrice;
+        this.extrasPrice = extrasPrice;
         Ingredients = ingredients;
     }
     #endregion
