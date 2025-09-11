630eeb719e4858934514aea71c756f2933bd2c5b
sarah-strawberries <blue.howrse@gmail.com>
2025-09-10 20:35:58 -0600

    removed comments from Location class

--- Cafeteria.Shared/Location.cs
+++ Cafeteria.Shared/Location.cs
@@ -5,18 +5,13 @@ public class Location
     #region Properties
     public string Name { get; init; }
     public string Address { get; init; }
-    // TODO: I'm not sure if we actually want these in here...and if we do, maybe there should be a list of DateTimes for the different days of the week
-    // DateTime DateTimeOpenMDT { get; set; } 
-    // DateTime DateTimeClosedMDT { get; set; }
     #endregion
 
     #region Constructor
-    public Location(string name, string address /*, DateTime open, DateTime closed*/)
+    public Location(string name, string address)
     {
         Name = name;
         Address = address;
-        // DateTimeOpenMDT = open;
-        // DateTimeClosedMDT = closed;
     }
     #endregion
 }
