e953e31d2097d142dc658a77cb1d98d51b088184
sarah-strawberries <blue.howrse@gmail.com>
2025-09-15 09:22:18 -0600

    made methods to get lists of dummy data stimulating getting lists from db tables

--- Cafeteria.Customer/Components/Data/DummyData.cs
+++ Cafeteria.Customer/Components/Data/DummyData.cs
@@ -1,55 +1,146 @@
 namespace Cafeteria.Customer.Components.Data;
+
+using System;
 using Cafeteria.Shared;
 
-public static class DummyData {
+public static class DummyData
+{
+    #region =========================== Ingredients Creation ==============================
+    public static Ingredient CreateWheatBread()
+    {
+        return new Ingredient("Wheat Bread", "https://picsum.photos/id/98/150/150", 0m);
+    }
 
+    public static Ingredient CreateOnion()
+    {
+        return new Ingredient("Onion", "https://picsum.photos/id/292/150/150", 0m);
+    }
+
+    public static Ingredient CreateLettuce()
+    {
+        return new Ingredient("Lettuce", "https://picsum.photos/id/189/150/150", 0m);
+    }
+
+    public static Ingredient CreateGoudaCheese()
+    {
+        return new Ingredient("Gouda Cheese", "https://picsum.photos/id/835/150/150", 0m);
+    }
+
+    public static Ingredient CreateRoastBeef()
+    {
+        return new Ingredient("Roast Beef", "https://picsum.photos/id/200/150/150", 0m);
+    }
+
+    public static Ingredient CreateMarbleRyeBread()
+    {
+        return new Ingredient("Marble Rye Bread", "https://picsum.photos/id/98/150/150", 0m);
+    }
+
+    public static Ingredient CreateGrilledChicken()
+    {
+        return new Ingredient("Grilled Chicken", "https://picsum.photos/id/200/150/150", 0m);
+    }
+
+    public static Ingredient CreateMozzarellaCheeseExtra()
+    {
+        return new Ingredient("Mozzarella Cheese", "https://picsum.photos/id/835/150/150", 0.79m);
+    }
+
+    public static Ingredient CreateAlfalfaSprouts()
+    {
+        return new Ingredient("Alfalfa Sprouts", "https://picsum.photos/id/400/150/150", 0m);
+    }
+    #endregion ==========================================================================
+
+
+
+    #region ========================= Ingredient Type Creation ============================
+    public static IngredientType CreateBreadType()
+    {
+        return new IngredientType("Bread");
+    }
+
+    public static IngredientType CreateMeatType()
+    {
+        return new IngredientType("Meat");
+    }
+
+    public static IngredientType CreateCheeseType()
+    {
+        return new IngredientType("Cheese");
+    }
+
+    public static IngredientType CreateOtherType()
+    {
+        return new IngredientType("Other");
+    }
+    #endregion ============================================================================
+
+
+
+    #region =========================== Food Item Creation ================================
     public static FoodItem GetDummySandwich()
     {
-        IReadOnlyDictionary<IngredientType, Ingredient> NormalSandwichIngredients = new Dictionary<IngredientType, Ingredient>()
+        IReadOnlyDictionary<IngredientType, Ingredient> normalSandwichIngredients = new Dictionary<IngredientType, Ingredient>()
         {
-            { new IngredientType("Bread"), new Ingredient("Wheat Bread", "https://picsum.photos/id/98/150/150", 0m) },
-            { new IngredientType("Meat"), new Ingredient("Roast Beef", "https://picsum.photos/id/200/150/150", 0m) },
-            { new IngredientType("Cheese"), new Ingredient("Gouda Cheese", "https://picsum.photos/id/835/150/150", 0m) },
-            { new IngredientType("Other"), new Ingredient("Lettuce", "https://picsum.photos/id/189/150/150", 0m) },
-            { new IngredientType("Other"), new Ingredient("Onion", "https://picsum.photos/id/292/150/150", 0m) }
+            { CreateBreadType(), CreateWheatBread() },
+            { CreateMeatType(), CreateRoastBeef() },
+            { CreateCheeseType(), CreateGoudaCheese() },
+            { CreateOtherType(), CreateLettuce() },
+            { CreateOtherType(), CreateOnion() }
         };
 
         return new FoodItem(
             name: "Test Sandwich",
             description: "A delicious test sandwich.",
             price: 5.99m,
-            imgUrl: "https://picsum.photos/id/488/150/150", 
-            ingredients: NormalSandwichIngredients
+            imgUrl: "https://picsum.photos/id/488/150/150",
+            ingredients: normalSandwichIngredients
         );
     }
 
     public static FoodItem GetDummySandwichWithExtraCheese()
     {
-        IReadOnlyDictionary<IngredientType, Ingredient> NormalSandwichIngredients = new Dictionary<IngredientType, Ingredient>()
+        IReadOnlyDictionary<IngredientType, Ingredient> sandwichIngredientsWithExtraCheese = new Dictionary<IngredientType, Ingredient>()
         {
-            { new IngredientType("Bread"), new Ingredient("Marble Rye Bread", "https://picsum.photos/id/98/150/150", 0m) },
-            { new IngredientType("Meat"), new Ingredient("Grilled Chicken", "https://picsum.photos/id/200/150/150", 0m) },
-            { new IngredientType("Cheese"), new Ingredient("Gouda Cheese", "https://picsum.photos/id/835/150/150", 0m) },
-            { new IngredientType("Cheese"), new Ingredient("Mozzarella Cheese", "https://picsum.photos/id/835/150/150", 0m) },
-            { new IngredientType("Other"), new Ingredient("Alfalfa Sprouts", "https://picsum.photos/id/400/150/150", 0m) },
+            { CreateBreadType(), CreateMarbleRyeBread() },
+            { CreateMeatType(), CreateGrilledChicken() },
+            { CreateCheeseType(), CreateGoudaCheese() },
+            { CreateCheeseType(), CreateMozzarellaCheeseExtra() }, // second piece of cheese adds $0.79
+            { CreateOtherType(), CreateAlfalfaSprouts() },
         };
-        
+
         return new FoodItem(
             name: "Test Sandwich With Extra Cheese",
             description: "A delicious test sandwich with mozzarella and gouda cheese!",
             price: 5.99m,
-            imgUrl: "https://picsum.photos/id/488/150/150", 
-            ingredients: NormalSandwichIngredients
+            imgUrl: "https://picsum.photos/id/488/150/150",
+            ingredients: sandwichIngredientsWithExtraCheese
         );
     }
 
     public static FoodItem GetDummyHamburger()
-    { 
+    {
         return new FoodItem(
             name: "Test Hamburger",
             description: "Hamburgers have default ingredients and don't need to go through the food builder.",
             price: 5.99m,
-            imgUrl: "https://picsum.photos/id/488/150/150" 
+            imgUrl: "https://picsum.photos/id/488/150/150"
         );
     }
+    #endregion ============================================================================
+
+
+
+    #region ============================== Menu Creation ==================================
+    public static List<FoodItem> GetDummyMenu()
+    {
+        return new List<FoodItem>()
+        {
+            GetDummySandwich(),
+            GetDummySandwichWithExtraCheese(),
+            GetDummyHamburger()
+        };
+    }
+    #endregion ============================================================================
 }
