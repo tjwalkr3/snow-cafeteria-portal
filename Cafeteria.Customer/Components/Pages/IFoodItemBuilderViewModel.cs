75cbf81debdefcae55b4696b6bf22db9bd1d3009
sarah-strawberries <blue.howrse@gmail.com>
2025-09-19 09:26:28 -0600

    created interface for food builder item vew model

---
+++ Cafeteria.Customer/Components/Pages/IFoodItemBuilderViewModel.cs
@@ -0,0 +1,15 @@
+using Cafeteria.Shared;
+
+namespace Cafeteria.Customer.Components.Pages;
+
+public interface IFoodItemBuilderViewModel
+{
+    FoodItem SelectedFoodItem { get; set; }
+    List<IngredientType> AvailableIngredientTypes { get; set; }
+    List<Ingredient> AvailableIngredients { get; set; }
+    List<Ingredient> SelectedIngredients { get; set; }
+    Dictionary<IngredientType, List<Ingredient>> IngredientsByType { get; set; }
+    void SelectIngredient(Ingredient ingredient);
+    void UnselectIngredient(Ingredient ingredient);
+    Dictionary<IngredientType, List<Ingredient>> GetIngredientsByType();
+}
