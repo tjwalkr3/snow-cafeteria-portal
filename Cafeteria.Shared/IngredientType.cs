66c8c224390650ae88be2cfbda6c4527f4e7e776
sarah-strawberries <blue.howrse@gmail.com>
2025-09-19 08:03:43 -0600

    updated ingredient type to enable knowing correct quantity

--- Cafeteria.Shared/IngredientType.cs
+++ Cafeteria.Shared/IngredientType.cs
@@ -3,9 +3,11 @@ namespace Cafeteria.Shared;
 public class IngredientType
 {
     public string Name { get; private set; }
+    public int Quantity { get; set; }
 
-    public IngredientType(string name)
+    public IngredientType(string name, int quantity)
     {
         Name = name;
+        Quantity = quantity;
     }
 }
