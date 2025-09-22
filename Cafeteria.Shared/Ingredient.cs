afb0275d8ca90bf564258f210152f5fd3b25d711
sarah-strawberries <blue.howrse@gmail.com>
2025-09-22 08:23:52 -0600

    added another constructor and made IngredientType nullable on Ingredient

--- Cafeteria.Shared/Ingredient.cs
+++ Cafeteria.Shared/Ingredient.cs
@@ -6,10 +6,31 @@ public class Ingredient
     public string Name { get; private set; }
     public string ImageUrl { get; set; }
     public decimal Price { get; private set; }
-    public IngredientType Type { get; set; }
+    public IngredientType? Type { get; set; }
     #endregion
 
-    #region Constructor
+    #region Constructors
+    /// <summary>
+    /// Creates an Ingredient object with a null IngredientType.
+    /// </summary>
+    /// <param name="name"></param>
+    /// <param name="imgUrl"></param>
+    /// <param name="price"></param>
+    public Ingredient(string name, string imgUrl, decimal price)
+    {
+        Name = name;
+        Price = price;
+        ImageUrl = imgUrl;
+    }
+
+    /// <summary>
+    /// Creates an ingredient with an IngredientType by accepting parameters for type name and quantity of IngredientType.
+    /// </summary>
+    /// <param name="name"></param>
+    /// <param name="imgUrl"></param>
+    /// <param name="ingredientType"></param>
+    /// <param name="quantityOfType"></param>
+    /// <param name="price"></param>
     public Ingredient(string name, string imgUrl, string ingredientType, int quantityOfType, decimal price)
     {
         Name = name;
