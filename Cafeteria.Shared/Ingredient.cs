800492938d4bb7911284a178595fa3c13722c718
sarah-strawberries <blue.howrse@gmail.com>
2025-09-22 08:12:52 -0600

    updated Ingredient and IngredientType dtos

--- Cafeteria.Shared/Ingredient.cs
+++ Cafeteria.Shared/Ingredient.cs
@@ -6,13 +6,15 @@ public class Ingredient
     public string Name { get; private set; }
     public string ImageUrl { get; set; }
     public decimal Price { get; private set; }
+    public IngredientType Type { get; set; }
     #endregion
 
     #region Constructor
-    public Ingredient(string name, string imgUrl, decimal price)
+    public Ingredient(string name, string imgUrl, string ingredientType, int quantityOfType, decimal price)
     {
         Name = name;
         Price = price;
+        Type = new IngredientType(ingredientType, quantityOfType);
         ImageUrl = imgUrl;
     }
     #endregion
--- Cafeteria.Shared/IngredientType.cs
+++ Cafeteria.Shared/IngredientType.cs
@@ -5,7 +5,7 @@ public class IngredientType
     public string Name { get; private set; }
     public int Quantity { get; set; }
 
-    public IngredientType(string name, int quantity)
+    public IngredientType(string name, int quantity = 1)
     {
         Name = name;
         Quantity = quantity;
