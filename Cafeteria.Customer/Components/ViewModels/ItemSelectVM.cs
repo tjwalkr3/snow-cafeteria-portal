f8f0ecee4aa956f5ad3799216b403816f1488d24
sarah-strawberries <blue.howrse@gmail.com>
2025-09-22 13:16:37 -0600

    fixed routing issue

--- Cafeteria.Customer/Components/ViewModels/ItemSelectVM.cs
+++ Cafeteria.Customer/Components/ViewModels/ItemSelectVM.cs
@@ -1,6 +1,12 @@
 ﻿namespace Cafeteria.Customer.Components.ViewModels;
 using Cafeteria.Customer.Components.ViewModelInterfaces;
+using Cafeteria.Customer.Components.Data;
+using Cafeteria.Shared;
 
-    public class ItemSelectVM : IItemSelectViewModel
+public class ItemSelectVM : IItemSelectViewModel
+{
+    public List<FoodItem> GetFoodItems()
     {
+        return DummyData.GetFoodItemList;
     }
+}
