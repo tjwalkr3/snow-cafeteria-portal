5201b1943f618f10c90116447314cf27e51f97b2
Thomas Jones <tjwalkr3@gmail.com>
2025-09-23 22:18:13 -0600

    stripped down the API and shared library, and added an empty controller for the menu

--- Cafeteria.Customer/DTOs/IngredientTypeDto.cs
+++
@@ -1,8 +0,0 @@
-namespace Cafeteria.Customer.DTOs;
-
-public class IngredientTypeDto
-{
-    public int Id { get; set; }
-    public string TypeName { get; set; } = string.Empty;
-    public int Quantity { get; set; }
-}
--- Cafeteria.Customer/DTOs/WeekDayDto.cs
+++
@@ -1,7 +0,0 @@
-namespace Cafeteria.Customer.DTOs;
-
-public class WeekDayDto
-{
-    public int Id { get; set; }
-    public string WeekdayName { get; set; } = string.Empty;
-}
