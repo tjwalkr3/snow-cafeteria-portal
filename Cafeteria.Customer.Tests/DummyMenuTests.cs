d652a4c7313975096a2607877c818c4fac342b86
sarah-strawberries <blue.howrse@gmail.com>
2025-10-03 13:01:40 -0600

    add test for dummy menu service to get dummy location list

---
+++ Cafeteria.Customer.Tests/DummyMenuTests.cs
@@ -0,0 +1,22 @@
+﻿using Cafeteria.Customer.Tests.DummyServices;
+using Cafeteria.Shared.DTOs;
+
+namespace Cafeteria.Customer.Tests;
+
+public class DummyMenuTests
+{
+    private DummyMenuService MenuService;
+
+    public DummyMenuTests()
+    {
+        MenuService = new DummyMenuService();
+    }
+
+    [Fact]
+    public async Task DummyMenuServiceReturnsDummyLocationList()
+    {
+        var locations = await MenuService.GetAllLocations();
+        List<LocationDto> locationList = locations.ToList();
+        Assert.True((locations is not null) && (locationList.Count > 0));
+    }
+}
