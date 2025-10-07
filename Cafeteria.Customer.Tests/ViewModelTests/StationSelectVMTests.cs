76670e27a5666901964674145da8bb63862dd628
sarah-strawberries <blue.howrse@gmail.com>
2025-10-07 15:54:47 -0600

    dotnet format :')

--- Cafeteria.Customer.Tests/ViewModelTests/StationSelectVMTests.cs
+++ Cafeteria.Customer.Tests/ViewModelTests/StationSelectVMTests.cs
@@ -6,33 +6,33 @@ using Cafeteria.Shared.Services;
 namespace Cafeteria.Customer.ViewModels.Tests;
 
 public class StationSelectVMTests
-{ 
-	[Fact]
-	public async Task ErrorOccurredWhileParsingSelectedLocation_ReturnsTrue_WhenUrlParsingFailedIsTrue()
-	{
-		// Arrange
-		var stationSelectVM = new StationSelectVM(new DummyMenuService()); // menu service isn't needed in this test
+{
+    [Fact]
+    public async Task ErrorOccurredWhileParsingSelectedLocation_ReturnsTrue_WhenUrlParsingFailedIsTrue()
+    {
+        // Arrange
+        var stationSelectVM = new StationSelectVM(new DummyMenuService()); // menu service isn't needed in this test
 
-		// Act
-		await stationSelectVM.GetDataFromRouteParameters(""); // pass a url with no query string so parsing fails
-		bool result = stationSelectVM.ErrorOccurredWhileParsingSelectedLocation();
+        // Act
+        await stationSelectVM.GetDataFromRouteParameters(""); // pass a url with no query string so parsing fails
+        bool result = stationSelectVM.ErrorOccurredWhileParsingSelectedLocation();
 
-		// Assert
-		Assert.True(result);
-	}
+        // Assert
+        Assert.True(result);
+    }
 
-	[Fact]
-	public void ErrorOccurredWhileParsingSelectedLocation_ReturnsFalse_WhenUrlParsingFailedIsFalse()
-	{
-		// Arrange
-		var stationSelectVM = new StationSelectVM(new DummyMenuService()); // menu service isn't needed in this test
+    [Fact]
+    public void ErrorOccurredWhileParsingSelectedLocation_ReturnsFalse_WhenUrlParsingFailedIsFalse()
+    {
+        // Arrange
+        var stationSelectVM = new StationSelectVM(new DummyMenuService()); // menu service isn't needed in this test
 
-		// Act
-		// Don't parse; error occurred should be false by default
-		bool result = stationSelectVM.ErrorOccurredWhileParsingSelectedLocation();
+        // Act
+        // Don't parse; error occurred should be false by default
+        bool result = stationSelectVM.ErrorOccurredWhileParsingSelectedLocation();
 
-		// Assert
-		Assert.False(result);
-	}
+        // Assert
+        Assert.False(result);
+    }
 
 }
