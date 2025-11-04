f0dd140e060ddf0bdbb5d72eb351b41d176a3918
Jesus Sunuc <mariano.code4guate@gmail.com>
2025-11-04 15:11:59 -0700

    clean up

--- Cafeteria.Customer/Components/Pages/Stations/BreakfastSwipe/IBreakfastSwipeVM.cs
+++ Cafeteria.Customer/Components/Pages/Stations/BreakfastSwipe/IBreakfastSwipeVM.cs
@@ -7,23 +7,23 @@ public interface IBreakfastSwipeVM
     List<EntreeDto> Entrees { get; }
     List<SideDto> Sides { get; }
     List<DrinkDto> Drinks { get; }
-    
+
     string ActiveTab { get; }
     EntreeDto? SelectedEntree { get; }
     SideDto? SelectedSide { get; }
     DrinkDto? SelectedDrink { get; }
     string? OrderConfirmation { get; }
-    
+
     string? SelectedMeatOption { get; }
     string? SelectedBreadOption { get; }
-    
+
     void SetActiveTab(string tab);
     void SelectEntree(EntreeDto entree);
     void SelectSide(SideDto side);
     void SelectDrink(DrinkDto drink);
     void SetMeatOption(string meat);
     void SetBreadOption(string bread);
-    
+
     bool RequiresMeatSelection(int entreeId);
     bool RequiresBreadSelection(int entreeId);
     int GetSelectionCount();
