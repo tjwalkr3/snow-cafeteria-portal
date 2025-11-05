abb66e43313610ca3df10650eeee5b7563c71ffb
Thomas Jones <tjwalkr3@gmail.com>
2025-11-05 14:51:22 -0700

    made the review order page correctly load the order from the cart and display it to the user

--- Cafeteria.Customer/Components/Pages/PlaceOrder/PlaceOrder.razor.cs
+++ Cafeteria.Customer/Components/Pages/PlaceOrder/PlaceOrder.razor.cs
@@ -19,24 +19,34 @@ public partial class PlaceOrder : ComponentBase
 
     private decimal Price { get; set; } = 0.0m;
 
-    protected override async Task OnInitializedAsync()
+    private bool _isLoading = true;
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
     {
-        // Test data that can be removed after the page before this is complete
-        string userName = "test";
-        var entree = new EntreeDto { Id = 1, EntreeName = "Burger", EntreePrice = 5.00m };
-        var side = new SideDto { Id = 1, SideName = "Fries", SidePrice = 3.00m};
-        var drink = new DrinkDto { Id = 1, DrinkName = "Coke" };
-        var option = new FoodOptionDto { Id = 1, FoodOptionName = "Tomato" };
-        var optionType = new FoodOptionTypeDto { Id = 1, FoodOptionTypeName = "Toppings", FoodOptionPrice = 0.50m };
+        if (firstRender)
+        {
+            // Test data that can be removed after the page before this is complete
+            string userName = "test";
+            if (await GetOrder(userName) == null) {
+                var entree = new EntreeDto { Id = 1, EntreeName = "Burger", EntreePrice = 5.00m };
+                var side = new SideDto { Id = 1, SideName = "Fries", SidePrice = 3.00m};
+                var drink = new DrinkDto { Id = 1, DrinkName = "Coke", DrinkPrice = 2.00m };
+                var option = new FoodOptionDto { Id = 1, FoodOptionName = "Tomato" };
+                var optionType = new FoodOptionTypeDto { Id = 1, FoodOptionTypeName = "Toppings", FoodOptionPrice = 0.50m };
 
-        await Cart.AddEntree(userName, entree);
-        await Cart.AddEntreeOption(userName, entree.Id, option, optionType);
-        await Cart.AddSide(userName, side);
-        await Cart.AddDrink(userName, drink);
+                await Cart.AddEntree(userName, entree);
+                await Cart.AddEntreeOption(userName, entree.Id, option, optionType);
+                await Cart.AddSide(userName, side);
+                await Cart.AddDrink(userName, drink);
+            }
 
-        // The code that gets the cart from local storage and calculates the cost
-        Order = await GetOrder(userName);
-        if (Order != null) Price = PlaceOrderVM.CalculateTotalPrice(Order);
+            // The code that gets the cart from local storage and calculates the cost
+            Order = await GetOrder(userName);
+            if (Order != null) Price = PlaceOrderVM.CalculateTotalPrice(Order);
+
+            _isLoading = false;
+            StateHasChanged();
+        }
     }
     
     private async Task<BrowserOrder?> GetOrder(string userName) 
