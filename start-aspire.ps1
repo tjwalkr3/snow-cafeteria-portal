165c658e43637dc65785a8060cc30b897e436a6f
Thomas Jones <tjwalkr3@gmail.com>
2025-11-08 15:47:36 -0700

    added a log line before the dashboard login line to make the scripts more user friendly

--- start-aspire.ps1
+++ start-aspire.ps1
@@ -1,3 +1,4 @@
 #!/usr/bin/env pwsh
 
+Write-Host "Starting Aspire AppHost with watch mode..." -ForegroundColor Cyan
 dotnet watch --project Cafeteria.AppHost/Cafeteria.AppHost.csproj 2>&1 | Select-String "Login to the dashboard"
