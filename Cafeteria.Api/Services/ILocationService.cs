612ff27cef0b5dd7d38b3457f1255a0aa3c517b9
sarah-strawberries <blue.howrse@gmail.com>
2025-12-01 13:18:49 -0700

    adjust nullability to work with 404 behavior of returning null objects

--- Cafeteria.Api/Services/ILocationService.cs
+++ Cafeteria.Api/Services/ILocationService.cs
@@ -9,12 +9,12 @@ namespace Cafeteria.Api.Services;
 public interface ILocationService
 {
     Task<List<LocationDto>> GetAllLocations();
-    Task<LocationDto> GetLocationByID(int locationId);
+    Task<LocationDto?> GetLocationByID(int locationId);
     Task CreateLocation(string name, string? description = null);
     Task UpdateLocationByID(int locationId, string name, string? description);
     Task DeleteLocationByID(int locationId);
     Task<List<LocationBusinessHoursDto>> GetLocationBusinessHours(int locationId);
-    Task<LocationBusinessHoursDto> GetLocationBusinessHoursById(int locationHrsId);
+    Task<LocationBusinessHoursDto?> GetLocationBusinessHoursById(int locationHrsId);
     Task AddLocationHours(int locationId, DateTime startTime, DateTime endTime, WeekDay weekday);
     Task UpdateLocationHoursById(int locationHrsId, DateTime startTime, DateTime endTime, WeekDay weekday);
     Task DeleteLocationHrsById(int locationHrsId);
--- Cafeteria.Api/Services/IStationService.cs
+++ Cafeteria.Api/Services/IStationService.cs
@@ -9,12 +9,12 @@ namespace Cafeteria.Api.Services;
 public interface IStationService
 {
     Task<List<StationDto>> GetStationsByLocation(int locationId);
-    Task<StationDto> GetStationByID(int stationId);
+    Task<StationDto?> GetStationByID(int stationId);
     Task CreateStationForLocation(int locationId, string stationName, string? stationDescription = null);
     Task UpdateStationByID(int stationId, string name, string? description);
     Task DeleteStationByID(int id);
     Task<List<StationBusinessHoursDto>> GetStationBusinessHours(int stationId);
-    Task<StationBusinessHoursDto> GetStationBusinessHoursById(int stationHrsId);
+    Task<StationBusinessHoursDto?> GetStationBusinessHoursById(int stationHrsId);
     Task AddStationHours(int stationId, DateTime startTime, DateTime endTime, WeekDay weekday);
     Task UpdateStationHoursById(int stationHrsId, DateTime startTime, DateTime endTime, WeekDay weekday);
     Task DeleteStationHrsById(int stationHrsId);
