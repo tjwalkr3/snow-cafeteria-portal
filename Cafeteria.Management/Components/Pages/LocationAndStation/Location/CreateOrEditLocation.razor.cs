ff1613390dadbc40eb589116ec2fa3897129e4d0
sarah-strawberries <blue.howrse@gmail.com>
2025-12-02 14:09:11 -0700

    initial create modal

--- Cafeteria.Management/Components/Pages/LocationAndStation/Location/CreateOrEditLocation.razor.cs
+++ Cafeteria.Management/Components/Pages/LocationAndStation/Location/CreateOrEditLocation.razor.cs
@@ -1,13 +1,40 @@
+using Cafeteria.Shared.DTOs;
 using Microsoft.AspNetCore.Components;
+
 namespace Cafeteria.Management.Components.Pages.LocationAndStation.Location;
 
 public partial class CreateOrEditLocation : ComponentBase
 {
-    [Inject]
-    public ICreateOrEditLocationVM ViewModel { get; set; } = null!;
+    [Parameter]
+    public LocationDto SelectedLocation { get; set; } = new();
+
+    private bool _isVisible;
 
     protected override async Task OnInitializedAsync()
     {
         await base.OnInitializedAsync();
+    }
+
+    public void Show()
+    {
+        _isVisible = true;
+        StateHasChanged();
+    }
+
+    public void Close()
+    {
+        _isVisible = false;
+        StateHasChanged();
+    }
+
+    public enum LocationModalState
+    {
+        Create,
+        Edit
+    }
+
+    public LocationModalState GetState()
+    {
+        return SelectedLocation.Id == 0 ? LocationModalState.Create : LocationModalState.Edit;
     }
 }
--- Cafeteria.Management/Components/Pages/LocationAndStation/LocationAndStation.razor.cs
+++ Cafeteria.Management/Components/Pages/LocationAndStation/LocationAndStation.razor.cs
@@ -1,11 +1,26 @@
 namespace Cafeteria.Management.Components.Pages.LocationAndStation;
 using Microsoft.AspNetCore.Components;
 using Cafeteria.Management.Services;
+using Cafeteria.Management.Components.Pages.LocationAndStation.Location;
+using Cafeteria.Shared.DTOs;
 
 public partial class LocationAndStation : ComponentBase
 {
     [Inject]
     public ILocationAndStationVM ViewModel { get; set; } = null!;
+
+    private CreateOrEditLocation _createOrEditLocationModal = null!;
+    private LocationDto _selectedLocation = new();
+
+    private void OpenCreateLocationModal()
+    {
+        _selectedLocation = new LocationDto
+        {
+            LocationName = "",
+            LocationDescription = ""
+        };
+        _createOrEditLocationModal.Show();
+    }
 
     private int? ExpandedLocationId { get; set; }
 
