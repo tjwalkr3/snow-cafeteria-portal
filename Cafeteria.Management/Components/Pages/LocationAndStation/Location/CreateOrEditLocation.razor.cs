d96e5b61029e9f59be0d64ebcbd6dc7f85a4414c
sarah-strawberries <blue.howrse@gmail.com>
2025-12-03 15:06:06 -0700

    remove partially implemented create functionality (included on child branch)

--- Cafeteria.Management/Components/Pages/LocationAndStation/Location/CreateOrEditLocation.razor.cs
+++ Cafeteria.Management/Components/Pages/LocationAndStation/Location/CreateOrEditLocation.razor.cs
@@ -1,40 +1,13 @@
-using Cafeteria.Shared.DTOs;
 using Microsoft.AspNetCore.Components;
-
 namespace Cafeteria.Management.Components.Pages.LocationAndStation.Location;
 
 public partial class CreateOrEditLocation : ComponentBase
 {
-    [Parameter]
-    public LocationDto SelectedLocation { get; set; } = new();
-
-    private bool _isVisible;
+    [Inject]
+    public ICreateOrEditLocationVM ViewModel { get; set; } = null!;
 
     protected override async Task OnInitializedAsync()
     {
         await base.OnInitializedAsync();
-    }
-
-    public void Show()
-    {
-        _isVisible = true;
-        StateHasChanged();
-    }
-
-    public void Close()
-    {
-        _isVisible = false;
-        StateHasChanged();
-    }
-
-    public enum LocationModalState
-    {
-        Create,
-        Edit
-    }
-
-    public LocationModalState GetState()
-    {
-        return SelectedLocation.Id == 0 ? LocationModalState.Create : LocationModalState.Edit;
     }
 }
--- Cafeteria.Management/Components/Pages/LocationAndStation/LocationAndStation.razor.cs
+++ Cafeteria.Management/Components/Pages/LocationAndStation/LocationAndStation.razor.cs
@@ -9,19 +9,6 @@ public partial class LocationAndStation : ComponentBase
     [Inject]
     public ILocationAndStationVM ViewModel { get; set; } = null!;
 
-    private CreateOrEditLocation _createOrEditLocationModal = null!;
-    private LocationDto _selectedLocation = new();
-
-    private void OpenCreateLocationModal()
-    {
-        _selectedLocation = new LocationDto
-        {
-            LocationName = "",
-            LocationDescription = ""
-        };
-        _createOrEditLocationModal.Show();
-    }
-
     private int? ExpandedLocationId { get; set; }
 
     private void ToggleLocation(int locationId)
