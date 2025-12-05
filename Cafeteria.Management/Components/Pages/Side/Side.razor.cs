f0f62253695a556dbee2b86a4fc29086fde14fa4
Thomas Jones <tjwalkr3@gmail.com>
2025-12-05 14:56:32 -0700

    the confirmation popup is now the component from the shared folder

--- Cafeteria.Management/Components/Pages/Side/Side.razor.cs
+++ Cafeteria.Management/Components/Pages/Side/Side.razor.cs
@@ -1,5 +1,4 @@
 using Microsoft.AspNetCore.Components;
-using Microsoft.JSInterop;
 using Cafeteria.Shared.DTOs;
 
 namespace Cafeteria.Management.Components.Pages.Side;
@@ -9,12 +8,15 @@ public partial class Side : ComponentBase
     [Inject]
     public ISideVM ViewModel { get; set; } = default!;
 
-    [Inject]
-    public IJSRuntime JSRuntime { get; set; } = default!;
-
     private bool ShowModal { get; set; }
     private bool IsEditMode { get; set; }
     private SideDto CurrentSide { get; set; } = new();
+
+    // Confirmation Modal State
+    private bool ShowConfirmation { get; set; }
+    private string ConfirmationTitle { get; set; } = "Confirm Delete";
+    private string ConfirmationMessage { get; set; } = "Are you sure you want to delete this side?";
+    private int _sideIdToDelete;
 
     protected override async Task OnInitializedAsync()
     {
@@ -54,12 +56,20 @@ public partial class Side : ComponentBase
         await ViewModel.LoadSidesAsync();
     }
 
-    private async Task DeleteSide(int id)
+    private void DeleteSide(int id)
     {
-        bool confirmed = await JSRuntime.InvokeAsync<bool>("confirm", "Are you sure you want to delete this side?");
-        if (confirmed)
-        {
-            await ViewModel.DeleteSideAsync(id);
-        }
+        _sideIdToDelete = id;
+        ShowConfirmation = true;
+    }
+
+    private async Task ConfirmDelete()
+    {
+        ShowConfirmation = false;
+        await ViewModel.DeleteSideAsync(_sideIdToDelete);
+    }
+
+    private void CancelDelete()
+    {
+        ShowConfirmation = false;
     }
 }
