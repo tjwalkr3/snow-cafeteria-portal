a94d527f0ccf32bf3de0eae3680285ee27c1b46f
sarah-strawberries <blue.howrse@gmail.com>
2025-12-05 12:22:33 -0700

    move logic from codebehind to view model for location create/edit modal

--- Cafeteria.Management/Components/Pages/LocationAndStation/Location/CreateOrEditLocationVM.cs
+++ Cafeteria.Management/Components/Pages/LocationAndStation/Location/CreateOrEditLocationVM.cs
@@ -1,5 +1,31 @@
+using Cafeteria.Shared.DTOs;
+
 namespace Cafeteria.Management.Components.Pages.LocationAndStation.Location;
 
 public class CreateOrEditLocationVM : ICreateOrEditLocationVM
 {
+    public LocationDto SelectedLocation { get; set; } = new();
+
+    public bool IsVisible { get; private set; }
+
+    public void Show()
+    {
+        IsVisible = true;
+    }
+
+    public void Close()
+    {
+        IsVisible = false;
+    }
+
+    public enum LocationModalState
+    {
+        Create,
+        Edit
+    }
+
+    public LocationModalState GetState()
+    {
+        return SelectedLocation.Id == 0 ? LocationModalState.Create : LocationModalState.Edit;
+    }
 }
--- Cafeteria.Management/Components/Pages/LocationAndStation/Location/ICreateOrEditLocationVM.cs
+++ Cafeteria.Management/Components/Pages/LocationAndStation/Location/ICreateOrEditLocationVM.cs
@@ -1,5 +1,12 @@
+using Cafeteria.Shared.DTOs;
+
 namespace Cafeteria.Management.Components.Pages.LocationAndStation.Location;
 
 public interface ICreateOrEditLocationVM
 {
+    LocationDto SelectedLocation { get; set; }
+    bool IsVisible { get; }
+    void Show();
+    void Close();
+    CreateOrEditLocationVM.LocationModalState GetState();
 }
