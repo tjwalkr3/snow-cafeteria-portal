0d3627b9e26e690a23ace996ec6be7f7ebe5a85b
Thomas Jones <tjwalkr3@gmail.com>
2025-12-05 13:21:56 -0700

    Added the ability to see the station name under each side in the Sides page

--- Cafeteria.Management/Components/Pages/Side/SideVM.cs
+++ Cafeteria.Management/Components/Pages/Side/SideVM.cs
@@ -6,10 +6,15 @@ namespace Cafeteria.Management.Components.Pages.Side;
 public class SideVM : ISideVM
 {
     private readonly ISideService _sideService;
+    private readonly ILocationService _locationService;
+    private readonly IStationService _stationService;
+    private Dictionary<int, string> _stationNames = new();
 
-    public SideVM(ISideService sideService)
+    public SideVM(ISideService sideService, ILocationService locationService, IStationService stationService)
     {
         _sideService = sideService;
+        _locationService = locationService;
+        _stationService = stationService;
     }
 
     public List<SideDto> Sides { get; private set; } = new();
@@ -17,6 +22,24 @@ public class SideVM : ISideVM
     public async Task LoadSidesAsync()
     {
         Sides = await _sideService.GetAllSides();
+        await LoadStationNamesAsync();
+    }
+
+    private async Task LoadStationNamesAsync()
+    {
+        var locations = await _locationService.GetAllLocations();
+        var tasks = locations.Select(l => _stationService.GetStationsByLocation(l.Id));
+        var stationsLists = await Task.WhenAll(tasks);
+
+        _stationNames = stationsLists
+            .SelectMany(s => s)
+            .DistinctBy(s => s.Id)
+            .ToDictionary(s => s.Id, s => s.StationName);
+    }
+
+    public string GetStationName(int stationId)
+    {
+        return _stationNames.TryGetValue(stationId, out var name) ? name : $"Station {stationId}";
     }
 
     public async Task DeleteSideAsync(int id)
