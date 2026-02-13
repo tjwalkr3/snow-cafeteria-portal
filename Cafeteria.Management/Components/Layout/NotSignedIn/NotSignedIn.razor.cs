4216880f45dfaa761fb02739fac67bf673253277
Thomas Jones <tjwalkr3@gmail.com>
2026-02-13 03:22:45 -0700

    removed the authorizeviews on all of the pages in the customer app, replaced them with a universal authorizeview, and made the mainlayout automatically redirect users to the signin page

---
+++ Cafeteria.Management/Components/Layout/NotSignedIn/NotSignedIn.razor.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Components;
+namespace Cafeteria.Management.Components.Layout.NotSignedIn;
+
+public partial class NotSignedIn : ComponentBase
+{
+    [Inject]
+    public NavigationManager NavigationManager { get; set; } = default!;
+
+    protected override void OnInitialized()
+    {
+        var currentUri = NavigationManager.ToAbsoluteUri(NavigationManager.Uri);
+
+        if (!currentUri.LocalPath.Equals("/signin", StringComparison.OrdinalIgnoreCase))
+        {
+            var returnUrl = Uri.EscapeDataString(currentUri.LocalPath);
+            NavigationManager.NavigateTo($"/signin?returnUrl={returnUrl}", forceLoad: true);
+        }
+    }
+}
